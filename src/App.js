9bc313559a9b80ff1c2d30ed15509bd070f0abd7
furkeyy706 <frk_snr@hotmail.com>
2024-04-11 18:07:08 +0300

    Finding lyrics with lyrics.ove api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import Axios from 'axios';
+
+import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faSearch, faMusic } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
 function App() {
+  const [artist, setArtist] = useState('');
+  const [song, setSong] = useState('');
+  const [lyrics, setLyrics] = useState('');
+  const [error, setError] = useState('');
+
+  function searchLyrics() {
+    if (artist === '' || song === '') {
+      setError('please fill in the artist name and song title fields');
+      return;
+    }
+    Axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+      .then(res => {
+        console.log(res.data.lyrics);
+        setLyrics(res.data.lyrics);
+        setError('');
+      })
+      .catch(error => {
+        console.error('!! Lyrics not found please try again.', error);
+        setLyrics('');
+        setError('!! Lyrics not found please try again.');
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <h1>
+        Lyrics Finder
+        <FontAwesomeIcon icon={faMusic} />
+      </h1>
+
+      <input
+        className="artist-input"
+        type="text"
+        placeholder="Artist name"
+        onChange={e => {
+          setArtist(e.target.value);
+        }}
+      />
+      <input
+        className="song-input"
+        type="text"
+        placeholder="Song name"
+        onChange={e => {
+          setSong(e.target.value);
+        }}
+      ></input>
+
+      <button className="search-button" onClick={() => searchLyrics()}>
+        <label className="icon">
+          <FontAwesomeIcon icon={faSearch} />
+        </label>
+        Search
+      </button>
+      <hr />
+      {error && <p className="error-message">{error}</p>}
+      <pre>{lyrics}</pre>
     </div>
   );
 }
